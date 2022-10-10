a427f3b20566ac52dd360b49f8651ebbb4d5e314
shikha <shikha1081998@gmail.com>
2022-10-10 16:42:22 +0530

    user model day 1

--- src/index.js
+++ src/index.js
@@ -3,24 +3,27 @@ const app = express()
 const route = require("./routes/route")
 const mongoose = require("mongoose")
 const multer = require("multer")
-require("dotenv").config()
-
 
 app.use(express.json())
 app.use(multer().any())
 
-mongoose.connect(process.env.MONGO_DBstring,{
+mongoose.connect("mongodb+srv://sonupk:1HivF6DXHWanVcYu@cluster0.vtjazgb.mongodb.net/group43Database",{
      useNewUrlParser: true 
     })
     .then(()=>console.log("MONGO-DB is connected on port 27017"))
     .catch((error)=>console.log(error))
 
+
+
 app.use("/",route)
+
+
 
 app.use((req, res) => { 
     res.status(404).send({ status: 'Error', message: "The Path you are requesting is not available !!" }); 
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`express app is running on ${process.env.PORT}`)
+
+app.listen(process.env.PORT||3000,()=>{
+    console.log(`express app is running on 3000`)
 })
